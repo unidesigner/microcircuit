6460cdc25a962b13b097a2baf25d06f169e40aef
Stephan Gerhard <git@unidesign.ch>
2011-09-14 11:46:16 +0200

    Added simulator with Petri Net idea

---
+++ microcircuit/simulator/__init__.py
@@ -0,0 +1,6 @@
+"""
+Inspired by the `Petri Net formalism <http://en.wikipedia.org/wiki/Petri_net>`_
+design a simulator where sensory events generate tokens that are propagated
+through the net. Connections correspond to places, vertices to transitions.
+
+"""
