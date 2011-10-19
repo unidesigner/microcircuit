27dc84ed6f7e52c0baeb2ae1529ba0321bafb77d
Stephan Gerhard <git@unidesign.ch>
2011-10-19 15:18:30 +0200

    Viz connectome

---
+++ doc/examples/showmatrix.py
@@ -0,0 +1,22 @@
+import microcircuit.constants as const
+from microcircuit.viz import show
+from microcircuit.dataset.testcircuit002 import testcircuit as tc
+from microcircuit.transforms.modularization import create_wiring_diagram
+
+connectome = create_wiring_diagram(tc)
+para = {
+    const.CONNECTOME_CHEMICAL_SYNAPSE: {
+        'marker': 'o',
+        'c': 'r',
+        's': 100
+    },
+    const.CONNECTOME_ELECTRICAL_SYNAPSE: {
+        'marker': '^',
+        'c': 'b',
+        's': (lambda x:x*100.)
+    }
+}
+nodes = connectome.graph.nodes()
+nodes = [111, 222, 333, 444, 555, 666]
+print nodes
+show(connectome, skeleton_order=nodes, use_label=True, display_parameters=para)
--- microcircuit/viz.py
+++ microcircuit/viz.py
@@ -1,5 +1,6 @@
 import networkx as nx
-from pylab import *
+import types
+import pylab
 
 # wrap fos display of circuit
 
@@ -8,11 +9,65 @@ from pylab import *
 
 # http://matplotlib.sourceforge.net/mpl_examples/pylab_examples/broken_barh.py
 
-def show(connectome):
+def show(connectome, skeleton_order, use_label, display_parameters):
 
     if connectome.graph is None:
         raise Exception("Connectome needs to have a valid graph")
 
-    m=nx.to_numpy_matrix(connectome.graph)
-    imshow( m, vmin=m.min(), vmax=m.max(), interpolation='nearest')
-    show()
+    labels = []
+    for ele in skeleton_order:
+        labels.append( connectome.metadata[ele]['name'] )
+    print "labels", labels
+
+    fig = pylab.figure()
+    ax = fig.add_subplot(111)
+
+    ax.set_ylabel('Neurons')
+    ax.set_yticks(range(len(labels)))
+    labels.reverse()
+    ax.set_yticklabels(labels)
+
+    ax.set_xlabel('Neurons')
+    ax.set_xticks(range(len(labels)))
+    labels.reverse()
+    ax.set_xticklabels(labels) # reverse back
+
+    for relation, para in display_parameters.items():
+
+        # scaling for each
+        if para.has_key('s'):
+            scale = para['s']
+        else:
+            scale = None
+        # color
+        if para.has_key('c'):
+            color = para['c']
+        else:
+            color = None
+        # marker
+        if para.has_key('marker'):
+            mark = para['marker']
+        else:
+            mark = None
+        x=[]; y=[]
+        if isinstance(scale,types.FunctionType):
+            scalelist = []
+        else:
+            scalelist = None
+        for u,v,data in connectome.graph.edges_iter(data=True):
+            if not (u in skeleton_order and v in skeleton_order):
+                continue
+            if data.has_key(relation):
+                x.append(skeleton_order.index(u))
+                y.append(skeleton_order.index(v))
+                if not scalelist is None:
+                    scalelist.append(scale(data[relation]))
+        y.reverse()
+        print x,y,scalelist,scale,color,mark
+
+        if scalelist is None and not scale is None: # TODO: correct?
+            pylab.scatter(x, y, s=scale, c=color,marker=mark)
+        else:
+            pylab.scatter(x, y, s=scalelist, c=color,marker=mark)
+
+    pylab.show()
